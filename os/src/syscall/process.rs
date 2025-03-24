1681e4f1eadee4a2becbd3c9afb83b15398c7392
Anekoique <ctolu01@gmail.com>
2025-03-25 01:03:51 +0800

    feat: fin ch4

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,9 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{
+    config::PAGE_SIZE, mm::{translated_byte_buffer, PageTable, PhysAddr, VirtAddr}, task::{
+        change_program_brk, current_user_token, exit_current_and_run_next, push_maparea, suspend_current_and_run_next, unmap_area, TASK_MANAGER
+    }, timer::get_time_us
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,28 +29,99 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let token = current_user_token();
+    let size = core::mem::size_of::<TimeVal>();
+    
+    let mut buffers = translated_byte_buffer(token, ts as *const u8, size);
+    
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    
+    let time_val_bytes = unsafe {
+        core::slice::from_raw_parts(
+            &time_val as *const _ as *const u8,
+            size
+        )
+    };
+    
+    let mut bytes_written = 0;
+    for buffer in buffers.iter_mut() {
+        let len = buffer.len().min(time_val_bytes.len() - bytes_written);
+        if len == 0 {
+            break;
+        }
+        buffer[..len].copy_from_slice(&time_val_bytes[bytes_written..bytes_written + len]);
+        bytes_written += len;
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    let page_table = PageTable::from_token(current_user_token());
+    let virtaddr = VirtAddr::from(id);
+    let pte = match page_table.translate(virtaddr.floor()) {
+        Some(pte) => pte,
+        None => return -1, 
+    };
+    match trace_request {
+        0 => {
+            if pte.is_user() && pte.readable() {
+                let physaddr: PhysAddr = pte.ppn().into();
+                let addr = physaddr.0 | virtaddr.page_offset();
+                let raw_ptr = addr as *const u8;
+                unsafe { *raw_ptr as isize }
+            } else {
+                -1
+            }
+        },
+        1 => {
+            if pte.is_user() && pte.writable() {
+                let physaddr: PhysAddr = pte.ppn().into();
+                let addr = physaddr.0 | virtaddr.page_offset();
+                let raw_ptr = addr as *mut u8;
+                unsafe {
+                    *raw_ptr = _data as u8;
+                    0
+                }
+            } else {
+                -1
+            }
+        },
+        2 => {
+            TASK_MANAGER.get_task_trace(id)
+        },
+        _ => {
+            panic!("sys_trace: Invalid trace_request!");
+        },
+    }
 }
 
 // YOUR JOB: Implement mmap.
-pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
-    trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
-    -1
+pub fn sys_mmap(start: usize, len: usize, prot: usize) -> isize {
+    trace!("kernel: sys_mmap");
+    if prot & !0x7 != 0 || prot & 0x7 == 0 { return -1; }
+    if !VirtAddr::from(start).aligned() { return -1; }
+
+    let len = (len + PAGE_SIZE - 1) & !(PAGE_SIZE - 1);
+    if !push_maparea(start, start + len, prot << 1) { return -1; }
+    0
 }
 
 // YOUR JOB: Implement munmap.
-pub fn sys_munmap(_start: usize, _len: usize) -> isize {
-    trace!("kernel: sys_munmap NOT IMPLEMENTED YET!");
-    -1
+pub fn sys_munmap(start: usize, len: usize) -> isize {
+    trace!("kernel: sys_munmap");
+    if !VirtAddr::from(start).aligned() { return -1; }
+    let len = (len + PAGE_SIZE - 1) & !(PAGE_SIZE - 1);
+    if !unmap_area(start, start + len) { return -1; }
+    0
 }
 /// change data segment size
 pub fn sys_sbrk(size: i32) -> isize {
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -17,6 +17,7 @@ mod task;
 use crate::loader::{get_app_data, get_num_app};
 use crate::sync::UPSafeCell;
 use crate::trap::TrapContext;
+use crate::mm::{MapPermission, VirtPageNum, VirtAddr};
 use alloc::vec::Vec;
 use lazy_static::*;
 use switch::__switch;
@@ -153,6 +154,44 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    /// add the task trace of syscall_id
+    pub fn task_trace_add(&self, id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].task_trace[id] += 1;
+    }
+
+    /// get the task trace number of syscall_id
+    pub fn get_task_trace(&self, id: usize) -> isize {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].task_trace[id] as isize
+    }
+
+    /// add a new Map_Area
+    pub fn push_maparea(&self, start_va: usize, end_va: usize, prot: usize) -> bool {
+        let mut inner = self.inner.exclusive_access();
+        let start_vpn = VirtPageNum::from(VirtAddr::from(start_va));
+        let end_vpn = VirtPageNum::from(VirtAddr::from(end_va));
+        let current = inner.current_task;
+        let memory_set = &mut inner.tasks[current].memory_set;
+        if !memory_set.check_range(start_vpn, end_vpn) { return false; }
+        memory_set.insert_framed_area(start_va.into(), end_va.into(), MapPermission::U | MapPermission::from_bits_truncate(prot as u8));
+        true
+    }
+
+    /// unmap an area 
+    pub fn unmap_area(&self, start_va: usize, end_va: usize) -> bool {
+        let mut inner = self.inner.exclusive_access();
+        let start_vpn = VirtPageNum::from(VirtAddr::from(start_va));
+        let end_vpn = VirtPageNum::from(VirtAddr::from(end_va));
+        let current = inner.current_task;
+        let memory_set = &mut inner.tasks[current].memory_set;
+        if !memory_set.check_area(start_vpn, end_vpn) { return false; }
+        memory_set.unmap_area(start_vpn);
+        true
+    }
 }
 
 /// Run the first task in task list.
@@ -201,4 +240,14 @@ pub fn current_trap_cx() -> &'static mut TrapContext {
 /// Change the current 'Running' task's program break
 pub fn change_program_brk(size: i32) -> Option<usize> {
     TASK_MANAGER.change_current_program_brk(size)
+}
+
+/// add a new Map_Area
+pub fn push_maparea(start_va: usize, end_va: usize, prot: usize) -> bool {
+    TASK_MANAGER.push_maparea(start_va, end_va, prot)
+}
+
+/// unmap an area 
+pub fn unmap_area(start_va: usize, end_va: usize) -> bool {
+    TASK_MANAGER.unmap_area(start_va, end_va)
 }
